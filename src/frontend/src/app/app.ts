b94265c0e5e29dc23113a60ddf49cdd0bed319dc
a-mahir-d <amd.coding.ac@gmail.com>
2026-06-09 22:08:34 +0300

    vercel analytics added

--- src/frontend/src/app/app.ts
+++ src/frontend/src/app/app.ts
@@ -1,7 +1,8 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Footer } from "./components/footer/footer";
 import { Navbar } from "./components/navbar/navbar";
+import { inject } from "@vercel/analytics"
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,10 @@ import { Navbar } from "./components/navbar/navbar";
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App {
+export class App implements OnInit{
   protected readonly title = signal('StockPulse');
+
+  ngOnInit(): void {
+    inject();
+  }
 }
