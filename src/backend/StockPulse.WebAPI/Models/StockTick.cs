ffec95e5cfef8cee6c2874c5cee4c64ad03e4390
a-mahir-d <amd.coding.ac@gmail.com>
2026-06-09 20:32:28 +0300

    bug fix

--- src/backend/StockPulse.WebAPI/Models/StockTick.cs
+++ src/backend/StockPulse.WebAPI/Models/StockTick.cs
@@ -1,8 +1,9 @@
 ﻿namespace StockPulse.WebAPI.Models;
 
-public class StockTick(string symbol, decimal initialPrice)
+public class StockTick(string symbol, decimal price)
 {
     public string Symbol { get; set; } = symbol;
-    public decimal Price { get; set; } = initialPrice;
+    public decimal Price { get; set; } = price;
     public DateTime LastUpdated { get; set; } = DateTime.UtcNow;
 }
+
